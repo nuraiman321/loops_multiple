ec7206c74703d2563bdca0a442fe8bc954aeabc8
nuraiman321 <nuraiman.gdb@gmail.com>
2022-10-15 12:25:09 +0800

    first commit

---
+++ loop_multiple_of_3.js
@@ -0,0 +1,16 @@
+let i = 100
+
+while ( i < 200  ){
+ i =+ i + 1
+ if( i % 3 == 0){
+
+     console.log(i);
+ }
+}
+
+
+// for(i = 100; i <= 200; i++){
+//     if (i % 3 == 0){
+//         console.log(i);
+//     }
+// }
---
+++ loop_multiple_of_3.py
@@ -0,0 +1,15 @@
+# i = 100
+# while i < 200 :
+#     if i % 3 == 0:
+#         print(i)
+#     i += 1
+
+# for i in range (100, 200, 3):
+#     print(i)
+    
+
+
+for i in range (4, 30, 4):
+    print(i)
+else:
+    print('finally done')
